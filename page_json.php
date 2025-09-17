7545c3abf00eac6255e74ce6ed1142b9ec796265
DavidJ-Saving-Time <david@atelierdjames.com>
2025-09-17 14:51:33 +0200

    Revert "Remove query string from citation URL"

--- page_json.php
+++ page_json.php
@@ -290,8 +290,7 @@ $docJson = json_encode($docData, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | 
         year = match[1];
       }
     }
-    const { origin, pathname, hash } = window.location;
-    const url = `${origin}${pathname}${hash ?? ''}`;
+    const url = window.location.href;
     const now = new Date();
     const accessed = now.toLocaleDateString('en-GB', {
       day: 'numeric',
